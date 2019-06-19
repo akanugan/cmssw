5e07f7182b851f60ca20b72d3cbe7edbf623590e
Ankush <ankushreddy_kanugan1@baylor.edu>
2019-06-19 17:24:25 +0200

    Changed type to bool

--- Validation/CaloTowers/python/CaloTowersParam_cfi.py
+++ Validation/CaloTowers/python/CaloTowersParam_cfi.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 calotowersAnalyzer = DQMEDAnalyzer('CaloTowersValidation',
-    outputFile               = cms.untracked.string(''),
-    CaloTowerCollectionLabel = cms.untracked.InputTag('towerMaker'),
-    hcalselector             = cms.untracked.string('all'),
-    mc                       = cms.untracked.string('yes')
-)
+                                   outputFile               = cms.untracked.string(''),
+                                   CaloTowerCollectionLabel = cms.untracked.InputTag('towerMaker'),
+                                   hcalselector             = cms.untracked.string('all'),
+                                   mc                       = cms.untracked.bool(True)
+                               )
 
 
 
